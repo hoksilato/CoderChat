7a30cde934779302ed62d4a484eddf6928a5d7a4
Cang Ta <hoksilato176@gmail.com>
2015-12-14 12:06:09 +0700

    change web-app name + fix some css and script

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -75,91 +75,6 @@ $(document).ready(function () {
 
     $(window).on('load resize', homepageResponsive);
 
-    /*  --------------
-         Menu Settings
-        --------------  */
-
-    // Hide Menu
-
-    $('.menu > div').on('click', function () {
-
-        var introWidth = $('.introduction').width(),
-            menuWidth = $('.menu').width();
-
-        $('.introduction').animate({
-            left: '-' + introWidth
-        }, 1000, 'easeOutQuart');
-        $('.menu').animate({
-            left: menuWidth
-        }, 1000, 'easeOutQuart', function () {
-            $('.home-page').css({
-                visibility: 'hidden'
-            });
-        });
-
-    });
-
-    // Show Reletive Page Onclick
-
-    $('.menu div.profile-btn').on('click', function () {
-        $('.profile-page').fadeIn(1200);
-        setTimeout(function(){
-            $('.count').each(function () {
-                $(this).prop('Counter',0).animate({
-                    Counter: $(this).text()
-                }, {
-                    duration: 7500,
-                    easing: 'swing',
-                    step: function (now) {
-                        $(this).text(Math.ceil(now));
-                    }
-                });
-            });
-        }, 100);
-    });
-
-    $('.menu div.portfolio-btn').on('click', function () {
-        $('.portfolio-page').fadeIn(1200);
-        setTimeout(function(){
-            $('#projects').mixItUp();
-        }, 100);
-    });
-
-    $('.menu div.service-btn').on('click', function () {
-        $('.service-page').fadeIn(1200);
-    });
-
-    $('.menu div.contact-btn').on('click', function () {
-        $('.contact-page').fadeIn(1200);
-        setTimeout(function(){
-            google.maps.event.trigger(map,'resize');
-        },100);
-    });
-
-
-    // Close Button, Hide Menu
-
-    $('.close-btn').on('click', function () {
-        $('.home-page').css({
-            visibility: 'visible'
-        });
-        $('.introduction, .menu').animate({
-            left: 0
-        }, 1000, 'easeOutQuart');
-        $('.profile-page, .portfolio-page, .service-page, .contact-page').fadeOut(800);
-    });
-
-    /*  ----------------------------------------
-         Tooltip Starter for Social Media Icons
-        ----------------------------------------  */
-
-    $('.intro-content .social-media [data-toggle="tooltip"]').tooltip({
-        placement: 'bottom'
-    });
-
-    $('.contact-details .social-media [data-toggle="tooltip"]').tooltip();
-
-
 
     /*----------------------script for owl carousel sponsors---------------------*/
 
@@ -187,8 +102,5 @@ $(document).ready(function () {
             speed: 500
         });
     });
-
-
-
 
 });
