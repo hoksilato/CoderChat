3776db545e373dc6f179f23238f263d4ecdb345d
Cang Ta <hoksilato176@gmail.com>
2015-12-14 14:42:43 +0700

    use preloader for only homepage

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,14 +1,3 @@
-
-/*  ------------------
-    Remove Preloader
-    ------------------  */
-
-$(window).load(function () {
-    $('#preloader').delay(350).fadeOut('slow', function () {
-        $('.profile-page, .portfolio-page, .service-page, .contact-page').hide();
-    });
-});
-
 $(document).ready(function () {
 
     'use strict';
